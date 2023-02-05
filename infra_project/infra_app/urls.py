e485c8083ff9f4507dda5ac954efbe43f0eb604e
Сергей Мартынов <40.10.444@gmail.com>
2023-02-05 14:33:02 +0300

    update version

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,12 +1,9 @@
+from django.urls import path
 from . import views
 
-from django.urls import path
-
-
-app_name = 'infra_app'
+app_name = "infra_app"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('second/', views.second_page, name='second_page'),
-
+    path("", views.index, name="index"),
+    path("second/", views.second_page, name="second_page"),
 ]
